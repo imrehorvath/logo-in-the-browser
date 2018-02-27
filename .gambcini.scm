4571546953299b23a2757804dbd82f87ec06d362
Imre Horvath <imi.horvath@gmail.com>
2018-02-27 21:05:31 +0100

    Update greeting text

--- .gambcini.scm
+++ .gambcini.scm
@@ -3,6 +3,6 @@
 (load "tables.scm")
 (load "logo.scm")
 (load "logo-meta.scm")
-(display "UCB Logo\"-ish\" Logo interpeter\nv0.2 2018, imi [dot] horvath [at] gmail [dot] com")
+(display ";; Logo interpeter implementing a subset of UCBLogo, minus the turtle graphics.\n;; v0.3 2018, imi [dot] horvath [at] gmail [dot] com\n;; Original project: https://github.com/imrehorvath/CS61A/tree/master/Logo")
 (newline)
 (initialize-logo)
