75693b123e510b065c849d468f21721960454eb6
Imre Horvath <imi.horvath@gmail.com>
2020-11-08 13:15:34 +0100

    Fix typo in greetings

--- .gambcini.scm
+++ .gambcini.scm
@@ -3,6 +3,6 @@
 (include "/obj.scm")
 (load "logo.scm")
 (load "logo-meta.scm")
-(display ";; This Logo interpeter implements a subset of UCBLogo, minus the turtle graphics.\n;; v1.0 2020, imi [dot] horvath [at] gmail [dot] com\n;; Library and examples taken from Berkeley Logo.\n;; Ymacs key bindings hint: C-S-y Paste from clipboard, M-S-w Copy to clipboard, C-x C-f find file\n;; Tips: load \" streams\n;; show.stream :fibs\n;; print choose 90 5")
+(display ";; This Logo interpeter implements a subset of UCBLogo, minus the turtle graphics.\n;; v1.0 2020, imi [dot] horvath [at] gmail [dot] com\n;; Library and examples taken from Berkeley Logo.\n;; Ymacs key bindings hint: C-S-y Paste from clipboard, M-S-w Copy to clipboard, C-x C-f find file\n;; Tips: load \"streams\n;; show.stream :fibs\n;; print choose 90 5")
 (newline)
 (initialize-logo)
