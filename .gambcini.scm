cc9853169027c6ac54a09c12ca0cea662a69a645
Imre Horvath <imi.horvath@gmail.com>
2020-11-24 14:19:16 +0100

    Cleanup Guile/Gambit initialization

--- .gambcini.scm
+++ .gambcini.scm
@@ -1,7 +1,9 @@
+;; Use this initialization to run the project in Gambit Scheme!
+
 (display ";; This Logo interpeter implements a subset of UCBLogo, minus the turtle graphics.\n;; v1.0 2020, imi [dot] horvath [at] gmail [dot] com\n;; Library and examples taken from Berkeley Logo.\n;; Ymacs key bindings hint: C-S-y Paste from clipboard, M-S-w Copy to clipboard, C-x C-f find file\n;; Tip: C-x C-f examples.txt, C-x 3, C-x b *scheme*")
 (newline)
 
-;; Gambit Scheme specific initialization
+;; Gambit Scheme-specific initialization
 
 (load "simply.scm")
 (load "tables.scm")
