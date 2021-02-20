55abc2600e25feb2f1961b308fa4907e475f5d0f
Imre Horvath <imi.horvath@gmail.com>
2021-02-20 21:26:55 +0100

    Update converter.py

--- converter.py
+++ converter.py
@@ -8,5 +8,5 @@ def by_chars(filename):
 if __name__ == "__main__":
     import sys, os, re
     path, filename = os.path.split(sys.argv[1])
-    location = re.sub(r'^.*Logo', '', path) + '/'  # setting Logo as root
+    location = re.sub(r'^.*Logo', '', path) + '/'  # Logo is the root
     print("Module['FS_createDataFile']('{}', '{}', {}, true, true);".format(location, filename, [ord(c) for c in by_chars(sys.argv[1])]))
